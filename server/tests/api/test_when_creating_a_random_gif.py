9c0f3b02f816312f162e011c90908262cb063bd7
Nick-Sullivan <nick.dave.sullivan@gmail.com>
2024-11-14 16:51:19 +0000

    fix a test

--- server/tests/api/test_when_creating_a_random_gif.py
+++ server/tests/api/test_when_creating_a_random_gif.py
@@ -1,29 +1,34 @@
-
 from io import BytesIO
 
 import pytest
-from PIL import Image, ImageChops
+from PIL import Image
 
 from .endpoints import create, get_image
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def response():
-    request = {'text': 'This is an API test', 'giphy_id': '', 'transparency': 200, 'version': 6}
+    request = {
+        "text": "This is an API test",
+        "giphy_id": "",
+        "transparency": 200,
+        "version": 6,
+        "is_boomerang": False,
+    }
     response = create(request)
     yield response
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def response_body(response):
     body = response.json()
     yield body
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def image(response_body) -> Image:
     try:
-        response = get_image(response_body['url'])
+        response = get_image(response_body["url"])
         byte_stream = BytesIO(response.content)
         yield Image.open(byte_stream)
     finally:
@@ -35,20 +40,20 @@ def test_it_returns_status_200(response):
 
 
 def test_it_returns_text(response_body):
-    assert 'text' in response_body
-    assert response_body['text'] == 'This is an API test'
+    assert "text" in response_body
+    assert response_body["text"] == "This is an API test"
 
 
 def test_it_returns_url(response_body):
-    assert 'url' in response_body
+    assert "url" in response_body
 
 
 def test_it_returns_id(response_body):
-    assert 'id' in response_body
-    
+    assert "id" in response_body
+
 
 def test_image_is_gif(image):
-    assert image.format == 'GIF'
+    assert image.format == "GIF"
 
 
 def test_image_size_is_as_expected(image):
